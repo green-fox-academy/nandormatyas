8fc540d1efd3381b8f6803371ea91e31f2c4c542
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 14:06:27 +0100

    feat(python):km to mile

---
+++ week-02/day-01/mile_to_km.py
@@ -0,0 +1,6 @@
+# Write a program that asks for an integer that is a distance in kilometers,
+# then it converts that value to miles and prints it
+
+km = float(input('Km?: '))
+
+print(km * 0.6214)
