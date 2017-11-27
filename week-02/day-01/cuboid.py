76774509ccefba7f630f22238fdb7fdfede1d33e
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 16:37:53 +0100

    feat(python): cuboid

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,13 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+
+length = int(input('Length: '))
+width = int(input('width: '))
+height = int(input('height: '))
+
+surface = (length + width + height) * 2
+volume = length * width * height
+
+print('surface area:', surface)
+print('volume: ', volume)
