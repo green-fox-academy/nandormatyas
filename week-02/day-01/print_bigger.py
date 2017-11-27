9acd5f245a5081c02b2dd2e677407a7530c11d79
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 16:51:43 +0100

    feat(python): print bigger number

---
+++ week-02/day-01/print_bigger.py
@@ -0,0 +1,9 @@
+# Write a program that asks for two numbers and prints the bigger one
+
+numberone = int(input('Number one: '))
+numbertwo = int(input('Number two: '))
+
+if numberone > numbertwo:
+  print(numberone)
+else:
+  print(numbertwo)
