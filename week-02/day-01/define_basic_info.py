eee46f254973368a5c85bc0e8d16d4771a7a40ea
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 12:40:55 +0100

    feat(python):basic info

---
+++ week-02/day-01/define_basic_info.py
@@ -0,0 +1,15 @@
+# Define several things as a variable then print their values
+# Your name as a string
+# Your age as an integer
+# Your height in meters as a double
+# Whether you are married or not as a boolean
+
+name = 'Nandi'
+age = int(32)
+height = float(1.75) * 2
+married = False
+
+print(name)
+print(age)
+print(height)
+print(married)
