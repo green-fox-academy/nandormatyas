8fd66c889580ef00ad9a0127bca84ff506f0c13b
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 12:31:11 +0100

    feat(python):favourite number

---
+++ week-02/day-01/favourite_number.py
@@ -0,0 +1,5 @@
+favorite_number = 8
+# Store your favorite number in a variable (as a number)
+# And print it like this: "My favorite number is: 8"
+
+print("my favourite number is: ", favorite_number)
