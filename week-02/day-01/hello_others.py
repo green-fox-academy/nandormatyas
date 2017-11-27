5bab447fc7bb77885f41e483b3e14d6ad4a35b16
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 12:15:51 +0100

    feat(python):Hello others

---
+++ week-02/day-01/hello_others.py
@@ -0,0 +1,10 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+# Hello, Esther!
+# Hello, Mary!
+# Hello, Joe!
+
+print("Hello, Laci!")
+print("Hello, Miki!")
+print("Hello, Bandi!")
