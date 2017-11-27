c4305d168f63ed5c6eaef5eca000bd7ea5edd32d
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 15:00:48 +0100

    feat(python):triangle

---
+++ week-02/day-01/triangle.py
@@ -0,0 +1,14 @@
+# Write a program that reads a number from the standard input, then draws a
+# triangle like this:
+#
+# *
+# **
+# ***
+# ****
+#
+# The triangle should have as many lines as the number was
+
+number = int(input('Enter number of triangle lines: '))
+
+for i in range(0, number, 1):
+  print('*' * (i + 1))
