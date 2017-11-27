6ff12ea896226a1ee0ad78dc2a6b80985294a184
nandormatyas <nandor.matyas@gmail.com>
2017-11-27 14:24:14 +0100

    feat(python): party indicator

---
+++ week-02/day-01/party-indicator.py
@@ -0,0 +1,26 @@
+# Write a program that asks for two numbers
+# Thw first number represents the number of girls that comes to a party, the
+# second the boys
+# It should print: The party is exellent!
+# If the the number of girls and boys are equal and there are more people coming than 20
+#
+# It should print: Quite cool party!
+# It there are more than 20 people coming but the girl - boy ratio is not 1-1
+#
+# It should print: Average party...
+# If there are less people coming than 20
+#
+# It should print: Sausage party
+# If no girls are coming, regardless the count of the people
+
+girls = int(input('How many girls?: '))
+boys = int(input('How many boys?:'))
+
+if girls == boys and (girls + boys) > 20:
+  print('The party is excellent!')
+elif (girls + boys) > 20 and girls >= 1:
+  print('Quite cool party')
+elif (girls + boys) < 20 and girls >= 1:
+  print('Average party')
+elif girls == 0:
+  print('Sausage party')
