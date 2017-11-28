8a309461cf15fc8ab987c687ce6e5a0a5025496b
nandormatyas <nandor.matyas@gmail.com>
2017-11-28 11:35:15 +0100

    feat(python- functions):sumup

---
+++ week-02/day-02/summa.py
@@ -0,0 +1,12 @@
+# - Write a function called `sum` that sum all the numbers
+#   until the given parameter
+
+def sum():
+  ranges = int(input('Sum until what number: '))
+  total = 0
+  for i in range(0, ranges, 1):
+   # print(i)
+    total += i
+  print(total)
+
+sum()
