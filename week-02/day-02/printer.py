cca8a798b400e3933ff43d99b7294066c932f090
nandormatyas <nandor.matyas@gmail.com>
2017-11-28 11:47:27 +0100

    feat(python-functions): printer)

---
+++ week-02/day-02/printer.py
@@ -0,0 +1,9 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer():
+  x = input('Enter anything you want me to tell you: ')
+  print(x)
+
+printer()
