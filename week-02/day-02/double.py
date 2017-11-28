ab4ee9247b5c41d92ccf7689884c0e48bcea6257
nandormatyas <nandor.matyas@gmail.com>
2017-11-28 11:21:37 +0100

    feat(python):double function

---
+++ week-02/day-02/double.py
@@ -0,0 +1,12 @@
+# - Create a variable named `ak` and assign the value `123` to it
+# - Create a function called `doubling` that doubles it's input parameter
+# - Print the result of `doubling(ak)`
+
+ak = 123
+
+def doubling(value):
+  value = value * 2
+
+  print(value)
+
+doubling(ak)
