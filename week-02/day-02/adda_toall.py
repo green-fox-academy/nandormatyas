4228e35ae8d4544b2d638f66c388a5bcb095d547
nandormatyas <nandor.matyas@gmail.com>
2017-11-28 12:53:50 +0100

    feat(python- lists): add a to all items

---
+++ week-02/day-02/adda_toall.py
@@ -0,0 +1,9 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macsk", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ["kuty", "macsk", "cic"]
+
+nimals[:] = [i + 'a' for i in nimals]
+
+print(nimals)
