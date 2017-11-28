c7e971491fd68ab7545f283d26eb9fba6a0e11cf
nandormatyas <nandor.matyas@gmail.com>
2017-11-28 15:07:34 +0100

    feat(python): matrix-diagonal

--- week-02/day-02/matrix-diagonal.py
+++ week-02/day-02/matrix-diagonal.py
@@ -8,17 +8,11 @@
 #
 # - Print this two dimensional list to the output
 
-n = 4
-a = [[0] * n]
-for i in range(n):
-  for j in range(n):
-    if i != j:
-      a[i][j] = 0
-      print(a)
-    else:
-      a[i][j] = 1
-      print(a)
 
 
-    
+for r in range(4):
+     for c in range(4):
+          print(int(c==r), end=" ")
+     print()
+
 
