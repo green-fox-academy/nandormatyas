03ddc70f51ca5f0d62340fcc8492e3f5234b74e2
nandormatyas <nandor.matyas@gmail.com>
2017-11-28 11:26:52 +0100

    feat(python- functions):appendA

---
+++ week-02/day-02/append.py
@@ -0,0 +1,12 @@
+# - Create a variable named `am` and assign the value `kuty` to it
+# - Write a function called `appendA` that gets a string as an input
+#   and appends an 'a' character to its end
+# - Print the result of `appendA(am)`
+
+am = 'kuty'
+
+def appendA(x):
+  x = x + 'a'
+  print(x)
+
+appendA(am)
