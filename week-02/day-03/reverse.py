ec98bbf3ad9e819af05d111b69e56fd33c32f97a
nandormatyas <nandor.matyas@gmail.com>
2017-11-29 09:44:13 +0100

    feat(python strings): first four exercise

---
+++ week-02/day-03/reverse.py
@@ -0,0 +1,11 @@
+# Create a function called 'reverse_string' which takes a string as a parameter
+# The function reverses it and returns with the reversed string
+
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+def reverse_string(x):
+  a = x[::-1]
+  print(a)
+
+reverse_string(reversed)
---
+++ week-02/day-03/simple_replace.py
@@ -0,0 +1,9 @@
+example = "In a dishwasher far far away"
+
+example = example[:5] + "galaxy" + example[15:]
+
+# I would like to replace "dishwasher" with "galaxy" in this example
+# Please fix it for me!
+# Expected ouput: In a galaxy far far away
+
+print(example)
