73cb28fddf82c99e35877974eade2f2457601c5e
nandormatyas <nandor.matyas@gmail.com>
2017-12-01 09:55:41 +0100

    feat(friday projects): guess my number updated

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -12,7 +12,7 @@ rang1, rang2 = input('In what range do you want to guess? (ex:1 100): ').split()
 rang1, rang2 = int(rang1), int(rang2)
 
 numberx = random.randrange(rang1, rang2)
-
+print('You have five lives')
 while lives > 0:
     guess = int(input('Guess the number: '))
     if guess < numberx:
@@ -32,7 +32,7 @@ while lives > 0:
 
 if lives == 0:
     print('Game Over')
-
+    print('The number was: ', numberx)
 
 
 
