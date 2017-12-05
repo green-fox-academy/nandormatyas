617b593b6246219a5fe6035cee71ba1b9784d943
nandormatyas <nandor.matyas@gmail.com>
2017-12-05 10:53:43 +0100

    feat(files): write multiple lines in file

--- week-03/day-02/write_multiple_lines.py
+++ week-03/day-02/write_multiple_lines.py
@@ -1,7 +1,9 @@
 def multiline_writer(path, word, number):
-    fopen = open(path, 'w')
-    fopen.write((word + "\n") * number)
-
+    try:
+        fopen = open(path, 'w')
+        fopen.write((word + "\n") * number)
+    except:
+        print('unable to write file')
 path = input('file? ')
 word = input('Word? ')
 number = int(input('Lines? '))
