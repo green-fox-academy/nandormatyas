06473a66302cf21b84b239389de4d8432c9b4b65
nandormatyas <nandor.matyas@gmail.com>
2017-12-06 11:04:10 +0100

    feat(drawing): square position

---
+++ week-03/day-03/position_square.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# create a square drawing function that takes 2 parameters:
+# the x and y coordinates of the square's top left corner
+# and draws a 50x50 square from that point.
+# draw 3 squares with that function.
+def draw_square(x, y):
+    square = canvas.create_rectangle(x, y, x + 50, y + 50)
+x = int(input('CoordinateX? '))
+y = int(input('CoordinateY? '))
+draw_square(x, y)
+root.mainloop()
