df845a2a76bafd63fdb42f85e5d65d868eed6a44
nandormatyas <nandor.matyas@gmail.com>
2017-12-06 10:37:07 +0100

    feat(drawing): line to the middle

---
+++ week-03/day-03/go_to_center.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# create a line drawing function that takes 2 parameters:
+# the x and y coordinates of the line's starting point
+# and draws a line from that point to the center of the canvas.
+# draw 3 lines with that function.
+
+def to_the_center(x, y):
+    
+    lines = canvas.create_line(x, y, 150, 150)
+
+x = input('CoordinateX? ')
+y = input('CoordinateY? ')
+to_the_center(x, y)
+
+root.mainloop()
