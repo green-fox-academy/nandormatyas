822b9cc721d24fa9d58418e137e9b3af68f72108
nandormatyas <nandor.matyas@gmail.com>
2018-01-08 11:40:23 +0100

    feat(JS): coding hours

---
+++ week-07/day-01/working_hours.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var daily_code = 6;
+var semester = 17;
+
+console.log('Total coding hours in one semester: ' + (semester * 5 * daily_code));
+console.log('Percent of coding hours: ' + (510 / (semester * 52)*100));
