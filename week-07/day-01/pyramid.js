6572a1a6b809196304692795713847b703712542
nandormatyas <nandor.matyas@gmail.com>
2018-01-08 14:35:29 +0100

    feat(JS):pyramid

--- week-07/day-01/pyramid.js
+++ week-07/day-01/pyramid.js
@@ -17,11 +17,8 @@ var pyramid = 10;
 
 var space = " ";
 var star = "*";
-//a = 0;
 
 for(var a = 0; a < pyramid; a++){
   console.log(space.repeat(pyramid - a - 1) + star.repeat(2 * a + 1));
 }
 
-//for (i in range(0, pyramid):
-  //print( space * (pyramid - i -1) + star * (2 * i + 1))
