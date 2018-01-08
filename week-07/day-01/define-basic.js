0c80c0124a3a1f58595cc8abe493fbe531678982
nandormatyas <nandor.matyas@gmail.com>
2018-01-08 11:52:02 +0100

    feat(JS): define-basic-info

---
+++ week-07/day-01/define-basic.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var name = 'Nandi';
+var age = 32;
+var height = 1.75;
+var married = false;
+
+console.log( name, age, height, married);
