01258793b04eede3e8db1043f67db2545a70f7e8
nandormatyas <nandor.matyas@gmail.com>
2018-01-08 14:43:04 +0100

    feat(JS): the diamond

--- week-07/day-01/diamond.js
+++ week-07/day-01/diamond.js
@@ -17,3 +17,17 @@ var lineCount = 7;
 //    *
 //
 // The diamond should have as many lines as lineCount is
+var star = '*';
+var space = ' ';
+for(var i = 0; i < lineCount - 1; i++){
+  console.log(space.repeat(lineCount - i) + star.repeat(2 * i + 1));
+}
+for(i; i >= 0; i--){
+  console.log(space.repeat(lineCount - i) + star.repeat(2 * i + 1));
+}
+
+
+//for idx in range(n-1):
+   // print((n-idx) * ' ' + (2*idx+1) * '*')
+//for idx in range(n-1, -1, -1):
+  //  print((n-idx) * ' ' + (2*idx+1) * '*')
