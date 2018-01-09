9e6d956a193ece967138e15d6fbfc12434d377ab
nandormatyas <nandor.matyas@gmail.com>
2018-01-09 15:20:08 +0100

    feat(JS): string addition

---
+++ week-07/day-02/todo-print.js
@@ -0,0 +1,15 @@
+'use strict';
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indention
+
+// Expected outpt:
+
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+
+var todoText = " - Buy milk\n";
+todoText = 'My Todo: \n' + todoText + ' - Download games \n' + '\t - Diablo'
+console.log(todoText);
