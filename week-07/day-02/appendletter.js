9a4a3bbb21fca54105973b6b324ee21283a2f398
nandormatyas <nandor.matyas@gmail.com>
2018-01-09 16:10:17 +0100

    feat(JS): append letter to all items in array

---
+++ week-07/day-02/appendletter.js
@@ -0,0 +1,8 @@
+'use strict';
+// Add "a" to every string in far
+
+var far = ["kuty", "macsk", "kacs", "rók", "halacsk"];
+for(var i = 0; i < far.length; i++){
+  far[i] += 'a';
+}
+console.log(far);
