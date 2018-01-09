c89f24b3442a50917e12b0c9ffb80ec194d029c2
nandormatyas <nandor.matyas@gmail.com>
2018-01-09 12:13:44 +0100

    feat(JS): mapping

---
+++ week-07/day-02/append-a.js
@@ -0,0 +1,8 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+var nimals = ["kuty", "macsk", "cic"];
+nimals = nimals.map(i => i + "a");
+console.log(nimals); 
---
+++ week-07/day-02/change-elements.js
@@ -0,0 +1,7 @@
+'use strict';
+// - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4 with the `.map` method 
+// - Print the fourth element as a test
+var s = [1, 2, 3, 8, 5, 6];
+var s = s.map(i => i == 8 ? 4 : i);
+console.log(s[3])
