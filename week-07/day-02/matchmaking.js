8cef238315cce8d7bb8fec798ed844437eeb9692
nandormatyas <nandor.matyas@gmail.com>
2018-01-09 16:06:32 +0100

    feat(JS):merge two arrays

---
+++ week-07/day-02/matchmaking.js
@@ -0,0 +1,11 @@
+'use strict';
+// Join the two array by matching one girl with one boy in the order array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+var girls = ["Eve","Ashley","Bözsi","Kat","Jane"];
+var boys = ["Joe","Fred","Béla","Todd","Neef","Jeff"];
+var order = [];
+for(var i = 0; i < girls.length;i++){
+  order.push(girls[i], boys[i]);
+}
+console.log(order);
