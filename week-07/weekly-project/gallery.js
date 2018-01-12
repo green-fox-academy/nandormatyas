e7f63bf08743619ab5d5043389fba2e3130f70f1
nandormatyas <nandor.matyas@gmail.com>
2018-01-12 11:52:18 +0100

    feat(Gallery): slide buttons working correctly

--- week-07/weekly-project/gallery.js
+++ week-07/weekly-project/gallery.js
@@ -40,6 +40,7 @@ var addSmallImages = document.querySelector('.small-images');
 for(var i = 0; i < pictures.length; i++){
   var image = document.createElement('img');
   image.setAttribute('src', pictures[i].path);
+  image.setAttribute('title', pictures[i].title);
   addSmallImages.appendChild(image);
 }
 
@@ -52,22 +53,39 @@ document.querySelector('#main').setAttribute('src', pathList[0]);
 
 var n = 0;
 var buttonLeft = document.querySelector('.left');
+buttonLeft.onmousedown = function(){
+  document.querySelector('.left').style.background = 'green';
+  document.querySelector('.left').style.boxShadow = 'inset -1px 2px 2px 1px black';
+}
+buttonLeft.onmouseup = function(){
+  document.querySelector('.left').style.background = 'lightgray';
+  document.querySelector('.left').style.boxShadow = '';
+}
 buttonLeft.onclick = function () {
-  if(n < 0 ){
+  if(n - 1 === - 1 ){
     n = pathList.length - 1;
-  }else if(n > pathList.length - 1){
-    n = 0;
+  }else{
+    n -= 1;
   }
-  document.querySelector('#main').setAttribute('src', pathList[n--]);
+  document.querySelector('#main').setAttribute('src', pathList[n]);
+
 }
 var buttonRight = document.querySelector('.right');
+buttonRight.onmousedown = function(){
+  document.querySelector('.right').style.background = 'green';
+  document.querySelector('.right').style.boxShadow = 'inset 1px 2px 2px 1px black';
+}
+buttonRight.onmouseup = function(){
+  document.querySelector('.right').style.background = 'lightgray';
+  document.querySelector('.right').style.boxShadow = '';
+}
 buttonRight.onclick = function () {
-  if(n < 0 ){
-    n = pathList.length - 1;
-  }else if(n > pathList.length - 1){
+  if(n + 1 === pathList.length){
     n = 0;
+  }else{
+    n += 1;
   }
-  document.querySelector('#main').setAttribute('src', pathList[n++]);
+  document.querySelector('#main').setAttribute('src', pathList[n]);
 }
 
 
