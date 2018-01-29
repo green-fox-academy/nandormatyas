5a5985c09f4cd9d0c7ce097759635168be27b8ba
nandormatyas <nandor.matyas@gmail.com>
2018-01-29 11:49:30 +0100

    feat(Prototypes): some basics

---
+++ week-10/day-01/01.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Create a constructor for creating Animals.
+// it should take one parameter: what the animal says
+// Every animal should have a method called say() that prints what the animal says
+
+function Animals (sound) {
+    this.sound = sound;
+}
+Animals.prototype.say = function () {
+  console.log(this.sound);
+}
+const dog = new Animals('woof');
+
+dog.say()
